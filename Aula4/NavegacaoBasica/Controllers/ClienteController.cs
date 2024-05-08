05fb818b75f3ea33a86e4ec008ae94f275dbc25c
Francisco Arlem <fmota350@gmail.com>
2024-05-07 22:39:44 -0300

    Inclusão Aula 4

---
+++ Aula4/NavegacaoBasica/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NavegacaoBasica.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Aula4/NavegacaoBasica/Controllers/ProdutoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NavegacaoBasica.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
