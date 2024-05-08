2994da2b42b71c54b38c88df4a0e03d339560729
Francisco Arlem <fmota350@gmail.com>
2024-05-07 22:38:58 -0300

    Inclusão Aula 2

---
+++ Aula2/MVCComTopLevel/Controllers/FuncionarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCComTopLevel.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Saudacao()
+        {
+            return View();
+        }
+    }
+}
---
+++ Aula2/MVCComTopLevel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCComTopLevel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
