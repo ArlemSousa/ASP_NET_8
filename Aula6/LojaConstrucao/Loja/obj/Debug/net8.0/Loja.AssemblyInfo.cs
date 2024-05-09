a72e17bcbe4f4dd22cdae4e6f856bc1862b3b246
Francisco Arlem <fmota350@gmail.com>
2024-05-09 19:39:07 -0300

    Aula 7 adicionada

--- Aula6/LojaConstrucao/Loja/obj/Debug/net8.0/Loja.AssemblyInfo.cs
+++ Aula6/LojaConstrucao/Loja/obj/Debug/net8.0/Loja.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0858d8bcd72b064ff67bf39a726d88186d9e44fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61faf3c7b78c5064e3b17ba26d03b3bd34dbf7e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
