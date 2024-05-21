5ca61d2a1504d301fcd41039d6c1bcf625d2c01d
Francisco Arlem <113395833+ArlemSousa@users.noreply.github.com>
2024-05-21 20:17:46 -0300

    Incremento da funcionalidade de filtro

---
+++ Aula12/MVCMovie/MVCMovie/Controllers/AdministratorsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCMovie.Data;
+using MVCMovie.Models;
+
+namespace MVCMovie.Controllers
+{
+    public class AdministratorsController : Controller
+    {
+        private readonly MVCMovieContext _context;
+
+        public AdministratorsController(MVCMovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Administrators
+        public async Task<IActionResult> Index(DateTime? startDate, DateTime? endDate)
+        {
+            var movies = from m in _context.Movie select m; //select * from movies
+
+            //se start tem algum valor, entra no if
+            if (startDate.HasValue)
+            {
+                movies = movies.Where(s => s.ReleaseDate >= startDate.Value); //where ReleaseDate >= startDate
+            }
+
+            //se end tem algum valor, entra no if
+            if (endDate.HasValue)
+            {
+                movies = movies.Where(s => s.ReleaseDate <= endDate.Value); //where ReleaseDate >= startDate
+            }
+
+
+            return View(await movies.ToListAsync());
+            //return View(await _context.Movie.ToListAsync());
+        }
+
+        // GET: Administrators/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Administrators/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administrators/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Administrators/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Administrators/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Administrators/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Administrators/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movie.Remove(movie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movie.Any(e => e.Id == id);
+        }
+    }
+}
--- Aula12/MVCMovie/MVCMovie/obj/Debug/net8.0/MVCMovie.AssemblyInfo.cs
+++ Aula12/MVCMovie/MVCMovie/obj/Debug/net8.0/MVCMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5da9a95a2727184356c26a7f3a7df6183e685f70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7e2d9a02d93bb31b9e54b163ae9e602059b8c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
